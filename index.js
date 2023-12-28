cf1c3d8e58362867a249baa5ad12f5e7545d5180
Улугбек Рашидов <@itstep.uz>
2023-12-28 15:29:47 +0500

    x

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ mongoose
   });
 const app = express();
 app.use(express.json());
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 const authenticateUser = async (req, res, next) => {
   try {
     const { email, password } = req.body;
