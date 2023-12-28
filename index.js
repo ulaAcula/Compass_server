622489e4a9d829b05103b4f8b8d0758193b214ab
Улугбек Рашидов <@itstep.uz>
2023-12-28 15:18:14 +0500

    corp fix

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ mongoose
 const app = express();
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://compass-server.onrender.com"
+  );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
